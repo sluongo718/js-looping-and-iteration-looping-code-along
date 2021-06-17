52a24d27793eab36600eb1c1cffdfe8fd68fc3c4
sluongo718 <sluongo718@yahoo.com>
2021-06-17 17:57:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+ const thanks = []
+    for (let i = 0; i < array.length; i++) {
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanks
+}
+
+function countDown(n) {
+    let countD = -1 
+    while (countD < n) {
+        console.log(n)
+        n--
+    }
+}
